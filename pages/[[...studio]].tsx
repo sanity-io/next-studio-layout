191d49d5b4ee35db1eb2d6f4e514650602464dcc
Cody Olsen <stipsan@gmail.com>
2022-08-06 16:10:19 +0200

    docs: open up demo in prod

--- pages/[[...studio]].tsx
+++ pages/[[...studio]].tsx
@@ -2,5 +2,11 @@ import config from 'sanity.config'
 import { StudioPageLayout } from 'src'
 
 export default function StudioPage() {
-  return <StudioPageLayout config={config} />
+  return (
+    <StudioPageLayout
+      config={config}
+      // Turn off login in production so that anyone can look around in the Studio and see how it works
+      unstable_noAuthBoundary={process.env.VERCEL_ENV === 'production'}
+    />
+  )
 }
