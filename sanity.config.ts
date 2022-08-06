45d425f54cb45a5f2057496be762876ae48024ac
Cody Olsen <stipsan@gmail.com>
2022-08-06 10:04:20 +0200

    chore: test endpoint

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,12 +3,15 @@ import { deskTool } from 'sanity/desk'
 
 import { schemaTypes } from './schemas'
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+
 export default createConfig({
   name: 'default',
   title: 'next-studio-layout',
 
-  projectId: 'pv8y60vp',
-  dataset: 'production',
+  projectId,
+  dataset,
 
   plugins: [deskTool()],
 
