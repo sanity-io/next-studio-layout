623ddbe93da6235d7df6d2d817d0140ba198fe24
Cody Olsen <stipsan@gmail.com>
2022-08-06 10:55:50 +0200

    debug api

--- pages/api/cors.ts
+++ pages/api/cors.ts
@@ -4,6 +4,7 @@ import { ServerTiming } from 'src/ServerTiming'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const token = process.env.SANITY_API_WRITE_TOKEN
 
 export default async function handler(
   req: NextApiRequest,
@@ -13,9 +14,43 @@ export default async function handler(
   serverTiming.start('handler')
 
   try {
+    if (!projectId) {
+      throw new Error('Missing NEXT_PUBLIC_SANITY_PROJECT_ID')
+    }
+    if (!dataset) {
+      throw new Error('Missing NEXT_PUBLIC_SANITY_DATASET')
+    }
+    if (!token) {
+      throw new Error('Missing SANITY_API_WRITE_TOKEN')
+    }
+
     const { host } = req.headers
 
-    /*
+    if (!host) {
+      throw new Error('Missing req.headers.host')
+    }
+
+    const vercelEnv = process.env.VERCEL_ENV
+    if (!vercelEnv) {
+      throw new Error('VERCEL_ENV is not set')
+    }
+
+    if (vercelEnv === 'development' && !host.startsWith('localhost:')) {
+      throw new Error('VERCEL_ENV is development but host is not localhost')
+    }
+
+    if (vercelEnv !== 'development') {
+      const vercelUrl = process.env.VERCEL_URL
+      if (!vercelUrl) {
+        throw new Error('VERCEL_URL is not set')
+      }
+      // Ignore URLs that are to specific deployments, otherwise we'd rack up CORS origins pretty fast
+      if (host === vercelUrl) {
+        throw new Error('VERCEL_URL is the same as host, ignoring')
+      }
+    }
+
+    /*=
     const client = createClient({
       projectId,
       dataset,
@@ -39,6 +74,13 @@ export default async function handler(
     // */
   } catch (err: any) {
     res.setHeader('Server-Timing', `${serverTiming}`)
-    return res.status(500).json({ message: err.message })
+    return res.status(500).json({
+      message: err.message,
+      VERCEK_ENV: process.env.VERCEL_ENV,
+      VERCEL_URL: process.env.VERCEL_URL,
+      'req.headers.host': req.headers.host,
+      projectId,
+      dataset,
+    })
   }
 }
