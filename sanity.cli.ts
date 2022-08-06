818e442550848ef6a0bb675fd71ab3ccff3998a0
Cody Olsen <stipsan@gmail.com>
2022-08-06 04:33:42 +0200

    chore: add sanity

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import { createCliConfig } from 'sanity/cli'
+
+export default createCliConfig({
+  api: {
+    projectId: 'pv8y60vp',
+    dataset: 'production',
+  },
+})
---
+++ sanity.config.ts
@@ -0,0 +1,17 @@
+import { createConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { schemaTypes } from './schemas'
+
+export default createConfig({
+  name: 'default',
+  title: 'next-studio-layout',
+
+  projectId: 'pv8y60vp',
+  dataset: 'production',
+
+  plugins: [deskTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
